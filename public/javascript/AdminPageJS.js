e05900c9589ef576f9ba96d761872a852df0175c
Ahmed Mohamed Abd Elsadek <148652667+Ahmed1234578@users.noreply.github.com>
2024-12-16 17:02:18 +0200

    Update AdminPageJS.js

--- public/javascript/AdminPageJS.js
+++ public/javascript/AdminPageJS.js
@@ -2,17 +2,7 @@ function myFunction() {
   document.getElementById("i2").classList.toggle("show");
 }
 
-function redirectToAdminProfile() {
-  window.location.href = "Profile.html";
-}
 
-function redirectToSharedDevices() {
-  window.location.href = "Devices.html";
-}
-
-function redirectToOrders() {
-  window.location.href = "Orders.html";
-}
 
 window.onclick = function(event) {
   if (!event.target.matches('.p3')) {
