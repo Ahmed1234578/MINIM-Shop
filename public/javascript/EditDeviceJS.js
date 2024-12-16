481186bf5151a20861ab849567e828d59e4d4d34
Ahmed Mohamed Abd Elsadek <148652667+Ahmed1234578@users.noreply.github.com>
2024-12-16 17:04:28 +0200

    Update EditDeviceJS.js

--- public/javascript/EditDeviceJS.js
+++ public/javascript/EditDeviceJS.js
@@ -1,6 +1,140 @@
 function EditFun() {
-  location.replace("../../views/admin/AdminPage.html")
-}
+  
+  
+  var prices=false;
+  var hdds=false;
+  var rams =false;
+  var screen_space_s=false;
+  var photos=false;
+  let number= /[0-9]/g;
+  let nameformat = /^[A-Za-z]+$/;
+  let sepChar= /[#.?!@$%^&*-]/g;
+  var numberformat2=/^\d$/;
+  let price=document.getElementById("PI").value;
+  let hdd=document.getElementById("HDDI").value;
+  let ram=document.getElementById("RAMI").value;
+  let screen_space=document.getElementById("SPI").value;
+  let photo=document.getElementById("PhI").value;
+    if(price==""){
+      document.getElementById("PV").innerHTML="Price must not be empty."
+    document.getElementById("PI").style.backgroundColor="#ffe6e6";
+    document.getElementById("PI").style.borderColor="red";
+    }else {
+      document.getElementById("PV").innerHTML=""
+        document.getElementById("PI").style.backgroundColor="white";
+        document.getElementById("PI").style.borderColor="black";
+      if(isNaN(price)){
+        document.getElementById("PV").innerHTML="Invalid price."
+    document.getElementById("PI").style.backgroundColor="#ffe6e6";
+    document.getElementById("PI").style.borderColor="red";
+      }else {
+        if(price<=0){
+          document.getElementById("PV").innerHTML="Invalid price."
+    document.getElementById("PI").style.backgroundColor="#ffe6e6";
+    document.getElementById("PI").style.borderColor="red";
+        }else {
+
+        
+        document.getElementById("PV").innerHTML=""
+        document.getElementById("PI").style.backgroundColor="white";
+        document.getElementById("PI").style.borderColor="black";
+        prices=true;
+        }
+      }
+    }
+    if(hdd==""){
+      document.getElementById("HV").innerHTML="Quantity field must not be empty."
+    document.getElementById("HDDI").style.backgroundColor="#ffe6e6";
+    document.getElementById("HDDI").style.borderColor="red";
+    }else {
+      document.getElementById("HV").innerHTML=""
+        document.getElementById("HDDI").style.backgroundColor="white";
+        document.getElementById("HDDI").style.borderColor="black";
+      if(isNaN(hdd)){
+        document.getElementById("HV").innerHTML="Invalid value."
+    document.getElementById("HDDI").style.backgroundColor="#ffe6e6";
+    document.getElementById("HDDI").style.borderColor="red";
+      }else {
+        if(hdd<=0){
+          document.getElementById("HV").innerHTML="Invalid value."
+    document.getElementById("HDDI").style.backgroundColor="#ffe6e6";
+    document.getElementById("HDDI").style.borderColor="red";
+        }else {
+
+        
+        document.getElementById("HV").innerHTML=""
+        document.getElementById("HDDI").style.backgroundColor="white";
+        document.getElementById("HDDI").style.borderColor="black";
+        hdds=true;
+        }
+      }
+    }
+    if(ram==""){
+      document.getElementById("RV").innerHTML="RAM size must not be empty."
+    document.getElementById("RAMI").style.backgroundColor="#ffe6e6";
+    document.getElementById("RAMI").style.borderColor="red";
+    }else {
+      document.getElementById("RV").innerHTML=""
+        document.getElementById("RAMI").style.backgroundColor="white";
+        document.getElementById("RAMI").style.borderColor="black";
+      if(isNaN(ram)){
+        document.getElementById("RV").innerHTML="Invalid value."
+    document.getElementById("RAMI").style.backgroundColor="#ffe6e6";
+    document.getElementById("RAMI").style.borderColor="red";
+      }else {
+        if(ram<=0){
+          document.getElementById("RV").innerHTML="Invalid value."
+    document.getElementById("RAMI").style.backgroundColor="#ffe6e6";
+    document.getElementById("RAMI").style.borderColor="red";
+        }else {
+
+         
+        document.getElementById("RV").innerHTML=""
+        document.getElementById("RAMI").style.backgroundColor="white";
+        document.getElementById("RAMI").style.borderColor="black";
+        rams=true;
+        }
+      }
+    }
+    if(screen_space==""){
+      document.getElementById("SPV").innerHTML="Screen space must not be empty."
+    document.getElementById("SPI").style.backgroundColor="#ffe6e6";
+    document.getElementById("SPI").style.borderColor="red";
+    }else {
+      document.getElementById("SPV").innerHTML=""
+        document.getElementById("SPI").style.backgroundColor="white";
+        document.getElementById("SPI").style.borderColor="black";
+      if(isNaN(screen_space)){
+        document.getElementById("SPV").innerHTML="Invalid space."
+    document.getElementById("SPI").style.backgroundColor="#ffe6e6";
+    document.getElementById("SPI").style.borderColor="red";
+      }else {
+        if(screen_space<=0){
+          document.getElementById("SPV").innerHTML="Invalid space."
+    document.getElementById("SPI").style.backgroundColor="#ffe6e6";
+    document.getElementById("SPI").style.borderColor="red";
+        }else {
+
+         
+        document.getElementById("SPV").innerHTML=""
+        document.getElementById("SPI").style.backgroundColor="white";
+        document.getElementById("SPI").style.borderColor="black";
+        screen_space_s=true;
+        }
+      }
+    }
+    if(photo==""){
+      document.getElementById("PHV").innerHTML="The photo is required"
+    }else {
+       document.getElementById("PHV").innerHTML=""
+       photos=true;
+    }
+    if(prices==true&&hdds==true&&rams==true&&screen_space_s==true&&photos==true){
+      location.replace("/views/ManageDevices.ejs")
+    }
+  }
+  
+
 function CanFun() {
-  location.replace("../../views/admin/AdminPage.html")
+  location.replace("/views/ManageDevices.ejs")
 }
